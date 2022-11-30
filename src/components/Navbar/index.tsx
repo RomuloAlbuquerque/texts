7d188c3cd8c989156fb7f6466bb9564c06087265
RomuloAlbuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2022-11-30 15:02:49 -0300

    adjusts in navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,33 +1,47 @@
-import './style.css'
 import { NavLink } from "react-router-dom";
-
+import "./style.css";
 
 const Navbar = () => {
   return (
-    <div className='container-main'>
-      <div className='container-brand'>
-        <h1>Romulo Albuquerque</h1>
+    <nav className="navbar navbar-expand-sm nav-container">
+      <div className="container-fluid">
+        <NavLink to="/">
+          <h4 className="nav-logo-text">Romulo Albuquerque</h4>
+        </NavLink>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbar-main"
+          aria-controls="navbar-main"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse container-list" id="navbar-main">
+          <ul className="navbar-nav offset-md-2 list-nav-main">
+            <li>
+              <NavLink to="/" activeClassName="active" exact>
+                <p>Home</p>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/text" activeClassName="active">
+                <p>Text</p>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/admin" activeClassName="active">
+                <p>Admin</p>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-    <div className="container-list-nav">
-      <ul className='list-group'>
-        <li>
-          <NavLink to="/" activeClassName="active my-link" exact>
-            <p>Home</p>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/text" activeClassName="active">
-            <p>Texto</p>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/feature" activeClassName="active">
-            <p>Feature</p>
-          </NavLink>
-        </li>
-      </ul>
-    </div>
-    </div>
+    </nav>
   );
 };
 
